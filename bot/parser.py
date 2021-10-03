2ef50645c0802e2d52a26b58a9013a1801e77430
nuno <nuno.ricardo.mars@gmail.com>
2021-10-03 11:44:09 +0200

    work in parser

--- bot/parser.py
+++ bot/parser.py
@@ -1,14 +1,19 @@
-from .constants import MENU_ITEMS
+from .constants import MENU_ITEMS, START_SESSION, CLOSE_SESSION
 
 
 def bot_parser(message):
     """
     Detect a command from the user and return the corresponding words.
     """
+    print(message)
     list_of_words = message.lower().split()
+    print(list_of_words)
+
+    if message in [START_SESSION, CLOSE_SESSION]:
+        return message
 
     for item in list_of_words:
-        if item  not in MENU_ITEMS:
+        if item not in MENU_ITEMS:
             return None            
-        else:
-            return " ".join(list_of_words[list_of_words.index(item) + 2:])
+
+        return item, " ".join(list_of_words[list_of_words.index(item) + 2:])
